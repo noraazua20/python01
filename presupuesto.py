bd603410960a11c80a206b9ae1c0fa5877ecb86b
Nora Azua <100675186+noraazua20@users.noreply.github.com>
2023-06-16 06:09:44 -0500

    Add files via upload

---
+++ presupuesto.py
@@ -0,0 +1,5 @@
+presupuesto_enero = 300
+presupuesto_febrero = 350
+presupuesto_marzo = 280
+promedio = (presupuesto_enero + presupuesto_febrero + presupuesto_marzo) / 3
+print("El promedio de el presupuesto trimestral es $", promedio)
